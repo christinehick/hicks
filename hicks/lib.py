122abaeacda02d224ed17fdd766ed20fb574bd9c
Christine Hick <c.hick@finance.brussels>
2022-05-23 12:13:28 +0200

    Initial Push

---
+++ hicks/lib.py
@@ -0,0 +1,6 @@
+#/home/hickchristine/code/christinehick/hicks
+
+def try_me():
+    print("What is your favourite Pizza?")
+    pizza = input()
+    print(pizza + " , good choice!")
---
+++ tests/test_lib.py
@@ -0,0 +1,4 @@
+from hicks.lib import try_me
+
+def try_me():
+    assert len(try_me()) != 0
